a9c0c9dc5be62ccfaafb9c15eabdecffe9234160
Chris Bauer <ShDragon@gmail.com>
2014-07-22 18:41:17 -0700

    Setting up Proxy classes

---
+++ src/main/java/com/shdragon/letsmodtest/reference/Reference.java
@@ -0,0 +1,9 @@
+package com.shdragon.letsmodtest.reference;
+
+
+public class Reference
+{
+    public static final String MOD_ID = "LetsModTest";
+    public static final String MOD_NAME = "Lets Mod Test";
+    public static final String VERSION = "1.7.2-1.0";
+}
